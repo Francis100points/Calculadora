1e0c2de35397f6665da8f89d7ff845e69148d815
Francis Castillo Fernández <franciscastillofernandez@gmail.com>
2022-05-16 10:37:31 +0200

    Añadido en Calculadora getter and setter

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -36,4 +36,38 @@ public class Calculadora {
     public Calculadora(){
         
     }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
 }
